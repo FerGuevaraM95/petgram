af157097447ffd8d2ee6702fc5ca901249f54d0c
Fernando Guevara <luisfernandoguevara95@gmail.com>
2019-09-28 23:48:07 -0500

    (Add): Logo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { ListOfCategories } from './components/ListOfCategories'
 import { GlobalStyle } from './GlobalStyles'
+import { Logo } from './Logo'
 import { ListOfPhotoCards } from './components/ListOfPhotoCards'
 
 export const App = () => (
   <div>
     <GlobalStyle />
+    <Logo />
     <ListOfCategories />
     <ListOfPhotoCards />
   </div>
