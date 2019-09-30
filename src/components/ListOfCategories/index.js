8a18dc24da11337febda9d70824c3448de0aea35
Fernando Guevara <luisfernandoguevara95@gmail.com>
2019-09-29 20:40:10 -0500

    (Add): Lista de categorias fixead

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,12 +1,13 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, Fragment } from 'react'
 import { Category } from '../Category'
 
 import { List, Item } from './styles'
 
 export const  ListOfCategories = () => {
   const [categories, setCategories] = useState([])
+  const [showFixed, setShowFixed] = useState(false)
 
-  useEffect(function()  {
+  useEffect(function() {
     fetch('https://petgram-server-gm.luisfernandoguevara95.now.sh/categories')
       .then(res => res.json())
       .then(response => {
@@ -14,12 +15,32 @@ export const  ListOfCategories = () => {
       })
   }, [])
 
-  return (
-    <List>
-      {categories.map(category => 
+  useEffect(function() {
+    const onScroll = e => {
+      const newShowFixed = window.scrollY > 200 
+        showFixed !== newShowFixed && 
+        setShowFixed(newShowFixed)
+    }
+
+    document.addEventListener('scroll', onScroll)
+
+    return () => document.removeEventListener('scroll', onScroll)
+  }, [showFixed])
+
+  const renderList = (fixed) => (
+    <List className={ fixed ? 'fixed' : '' }>
+      {categories.map(category =>
         <Item key={category.id}>
           <Category {...category} />
         </Item>)}
     </List>
+  )
+
+  return (
+    <Fragment>
+      { renderList() }
+      { showFixed && renderList(true) }
+    </Fragment>
+
   )
 }
--- src/components/ListOfCategories/styles.js
+++ src/components/ListOfCategories/styles.js
@@ -1,10 +1,26 @@
 import styled from 'styled-components'
+import { fadeIn } from '../../styles/animation'
 
 export const List = styled.ul`
   display: flex;
   overflow: scroll;
   width: 100%;
+  &.fixed {
+    background: #fff;
+    border-radius: 60px;
+    box-shadow: 0 0 20px rgba(0, 0, 0, 0.3);
+    top: -20px;
+    left: 0;
+    right: 0;
+    margin: 0 auto;
+    max-width: 400px;
+    padding: 5px;
+    position: fixed;
+    transform: scale(.5);
+    z-index: 1;
+    ${fadeIn()}
+  }
 `
 export const Item = styled.li`
-  padding: 0 8;
+  padding: 0 8px;
 `
