7a85d2fa0c5f33af868033c916e41d499e7c36ef
Fernando Guevara <luisfernandoguevara95@gmail.com>
2019-10-22 21:10:51 -0500

    (Add): Renderizado de una Foto card en especifico con render props

--- src/App.js
+++ src/App.js
@@ -3,12 +3,25 @@ import { ListOfCategories } from './components/ListOfCategories'
 import { GlobalStyle } from './styles/GlobalStyles'
 import { Logo } from './Logo'
 import { ListOfPhotoCards } from './container/ListOfPhotoCards'
+import { PhotoCardWithQuery } from './container/PhotoCardWithQuery'
 
-export const App = () => (
-  <div>
-    <GlobalStyle />
-    <Logo />
-    <ListOfCategories />
-    <ListOfPhotoCards categoryId={2} />
-  </div>
-)
+export const App = () => {
+  const urlParams = new window.URLSearchParams(window.location.search)
+  const detailId = urlParams.get('detail')
+  console.log(detailId)
+
+  return (
+    <div>
+      <GlobalStyle />
+      <Logo />
+      {
+        detailId
+          ? <PhotoCardWithQuery id={detailId} />
+          : <>
+            <ListOfCategories />
+            <ListOfPhotoCards categoryId={1} />
+            </>
+      }
+    </div>
+  )
+}
