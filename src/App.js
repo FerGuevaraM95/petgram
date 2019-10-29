94b1f6a864cf1720c7786e8a4fdb260dd60bb45e
Fernando Guevara <luisfernandoguevara95@gmail.com>
2019-10-28 19:22:34 -0600

    (Add): Navbar

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { GlobalStyle } from './styles/GlobalStyles'
 import { Logo } from './Logo'
 import { Home } from './pages/Home'
 import { Detail } from './pages/Detail'
+import { Navbar } from './components/Navbar'
 
 export const App = () => {
   return (
@@ -16,6 +17,7 @@ export const App = () => {
         <Home path='/pet/:categoryId' />
         <Detail path='/detail/:detailId' />
       </Router>
+      <Navbar />
     </div>
   )
 }
