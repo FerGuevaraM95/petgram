588ec61a4ab57d97cf93ce109df0e7e110d089f8
Fernando Guevara <luisfernandoguevara95@gmail.com>
2019-11-04 21:52:14 -0600

    (Add): Formulario de Usuario

--- src/pages/NotRegisteredUser.js
+++ src/pages/NotRegisteredUser.js
@@ -1,15 +1,17 @@
 import React from 'react'
 
 import Context from '../Context'
+import { UserForm } from '../components/UserForm'
 
 export const NotRegisteredUser = () => (
   <Context.Consumer>
     {
       ({ activateAuth }) => {
         return (
-          <form onSubmit={activateAuth}>
-            <button>Iniciar Sesión</button>
-          </form>
+          <>
+            <UserForm title='Registrarse' onSubmit={activateAuth} />
+            <UserForm title='Iniciar Sesión' onSubmit={activateAuth} />
+          </>
         )
       }
     }
