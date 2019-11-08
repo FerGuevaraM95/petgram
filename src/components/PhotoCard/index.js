369bf16c826bfa4d1b07adaeab06fcdb101d51fe
Fernando Guevara <luisfernandoguevara95@gmail.com>
2019-11-07 22:39:42 -0600

    (Add): Hacer like como usuario registrado

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,18 +1,15 @@
-import React, { Fragment, useEffect, useRef, useState } from 'react'
+import React from 'react'
 import { Link } from '@reach/router'
 
 import { Article, ImgWrapper, Img } from './styles'
-import { UseLocalStorage } from '../../hooks/useLocalStorage'
 import { useNearScreen } from '../../hooks/useNearScreen'
 import { TogleLikeMutation } from '../../container/TogleLikeMutation'
 import { FavButton } from '../FavButton'
 
 const DEFAULT_IMAGE = 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
 
-export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
+export const PhotoCard = ({ id, liked, likes = 0, src = DEFAULT_IMAGE }) => {
   const [show, element] = useNearScreen()
-  const key = `like-${id}`
-  const [liked, setLiked] = UseLocalStorage(key, false)
 
   return (
     <Article ref={element}>
@@ -27,12 +24,11 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
             {
               (togleLike) => {
                 const handleFavClick = () => {
-                  !liked && togleLike({
+                  togleLike({
                     variables: {
                       input: { id }
                     }
                   })
-                  setLiked(!liked)
                 }
                 return (
                   <FavButton
