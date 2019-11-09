a4c34100427f896683235d06a183c89602318c10
Fernando Guevara <luisfernandoguevara95@gmail.com>
2019-11-08 22:40:10 -0600

    (Add): Mostrar favoritos

--- src/pages/Favs.js
+++ src/pages/Favs.js
@@ -1,5 +1,9 @@
 import React from 'react'
+import { FavsWichQuery } from '../container/GetFavorites'
 
 export const Favs = () => (
-  <h1>Favs</h1>
+  <>
+    <h1>Favs</h1>
+    <FavsWichQuery />
+  </>
 )
