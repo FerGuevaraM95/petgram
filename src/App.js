bd6f27ee34e93b15aec30e6676c640575d391cd8
Fernando Guevara <luisfernandoguevara95@gmail.com>
2019-11-09 10:53:25 -0600

    (Refactor): Redirecciones de rutas

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { Router } from '@reach/router'
+import React, { useContext } from 'react'
+import { Router, Redirect } from '@reach/router'
 
 import { GlobalStyle } from './styles/GlobalStyles'
 import { Home } from './pages/Home'
@@ -7,34 +7,30 @@ import { Detail } from './pages/Detail'
 import { Favs } from './pages/Favs'
 import { User } from './pages/User'
 import { NotRegisteredUser } from './pages/NotRegisteredUser'
+import { NotFound } from './pages/NotFound'
 import { Logo } from './Logo'
 import { Navbar } from './components/Navbar'
-import Context from './Context'
+import { Context } from './Context'
 
 export const App = () => {
+  const { isAuth } = useContext(Context)
   return (
     <div>
       <GlobalStyle />
       <Logo />
       <Router>
+        <NotFound default />
         <Home path='/' />
         <Home path='/pet/:categoryId' />
         <Detail path='/detail/:detailId' />
+        {!isAuth && <NotRegisteredUser path='/login' />}
+        {!isAuth && <Redirect from='/favs' to='/login' noThrow />}
+        {!isAuth && <Redirect from='/user' to='/login' noThrow />}
+        {isAuth && <Redirect from='login' to='/' noThrow />}
+        <Favs path='/favs' />
+        <User path='/user' />
       </Router>
-      <Context.Consumer>
-        {
-          ({ isAuth }) =>
-            isAuth
-              ? <Router>
-                <Favs path='/favs' />
-                <User path='/user' />
-              </Router>
-              : <Router>
-                <NotRegisteredUser path='/favs' />
-                <NotRegisteredUser path='/user' />
-              </Router>
-        }
-      </Context.Consumer>
+
       <Navbar />
     </div>
   )
---
+++ src/pages/NotFound.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export const NotFound = () => (
+  <h1>Esta página no esixte :(</h1>
+)
